b8b88f28cd4515758311b0c12b393e93ce648330
TheKing-Kodi <evansataz@hotmail.com>
2017-07-15 15:44:44 -0700

    update

--- _repo/plugin.video.area51/default.py
+++ _repo/plugin.video.area51/default.py
@@ -36,7 +36,7 @@ def run():
     if not kasutajanimi:
         kasutajanimi = "NONE"
         salasona="NONE"
-    lehekylg="http://server.iptv-area-51.tv"
+    lehekylg="http://iptv-area-51.tv"
     pordinumber="2095"
     uuendused=plugintools.get_setting(sync_data("dXVlbmR1c2Vk"))
     vanemalukk=plugintools.get_setting(sync_data("dmFuZW1hbHVraw=="))
